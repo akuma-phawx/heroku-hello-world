01d8f0e2ab23d3d1e6733ec95cf6c81e0573907b
akuma-phawx <chrisvradis00@gmail.com>
2021-02-26 14:41:44 +0200

    listening to /

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.get("/", (req, res) => {
+  res.send("Welcome to /");
+});
+
 app.listen(PORT, () => {
   console.log(`Server started and listening on port ${PORT}`);
 });
